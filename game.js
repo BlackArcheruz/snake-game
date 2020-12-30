01ebbca80530063c2ee6eb6f8f55cc307f8b60e9
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2020-12-30 15:46:07 +0500

    Update snake

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
 let lastRenderTime = 0;
+const gameBoard = document.getElementById('game-board');
 
 
 function main(currentTime){
@@ -20,5 +21,6 @@ function update(){
 }
 
 function draw(){
-    drawSnake();
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
 }
